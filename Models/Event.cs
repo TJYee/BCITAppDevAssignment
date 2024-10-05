c75f7bc28311b4e0d8d9fa11ab164c2c5fce31d1
Taylor Y. <TJYee@users.noreply.github.com>
2024-10-05 16:22:36 -0700

    Created Models.

---
+++ Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BCITAppDevAssignment.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Models/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BCITAppDevAssignment.Models
+{
+    public class Registration
+    {
+        public int RegistrationID { get; set; }
+        public int EventID { get; set; }
+        public int ParticipantID { get; set; }
+        public string AdditionalInformation { get; set; }
+
+        public virtual Event Event { get; set; }
+        public virtual Participant Participant { get; set; }
+    }
+}
