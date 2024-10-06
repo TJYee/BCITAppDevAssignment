0c6394b34840207ea61b7b45537b9ec68dfda6e9
Taylor Y. <TJYee@users.noreply.github.com>
2024-10-06 16:57:25 -0700

    Started Participant View and Controller.

---
+++ Controllers/ParticipantController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BCITAppDevAssignment;
+
+namespace BCITAppDevAssignment.Controllers
+{
+    public class ParticipantController : Controller
+    {
+        private CEMS_To_DB_Context db = new CEMS_To_DB_Context();
+        // GET: Participant
+        public ActionResult Index()
+        {
+            return View(db.Events.ToList());
+        }
+
+        // GET: Participant/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Participant/Register/5
+        public ActionResult Register(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+    }
+}
